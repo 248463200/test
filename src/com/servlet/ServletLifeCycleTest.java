074bccf1ba1eeb9980e0517ce28e35c97ed2809d
peng_yaohui <248463200@qq.com>
2021-01-09 23:17:24 +0800

    servlet test

---
+++ src/com/servlet/ServletLifeCycleTest.java
@@ -0,0 +1,41 @@
+package com.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class ServletLifeCycleTest implements Servlet {
+
+
+    public ServletLifeCycleTest(){
+        System.out.println("ServletLifeCycleTest  execute");
+    }
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println("ServletLifeCycleTest  init execute");
+    }
+
+
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("ServletLifeCycleTest service execute");
+    }
+
+
+
+    @Override
+    public void destroy() {
+        System.out.println("ServletLifeCycleTest destory execute");
+    }
+
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+}
